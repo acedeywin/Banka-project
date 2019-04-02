a48851d32511ff8d87cdd443b14f461903935393
acedeywin <stephenokpala@gmail.com>
2019-04-02 05:50:04 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+//Math.floor(1000 + Math.random() * 9000)
 
-const express = require('express'),
-      path = require('path');
+
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
 
 //set up express app
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'UI')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-
+app.get('/admin', (req, res) => {
+    res.render('create-account');
+})
 
 //listen for request
-const server = app.listen(process.env.port || 3000, () => {
+app.listen(process.env.port || 3000, () => {
     console.log('Server is up');
 });
 
