e01b510125ab72c85da6733d4de0285431850c61
acedeywin <stephenokpala@gmail.com>
2019-04-26 22:43:56 +0100

    feature(account-update):admin and staff to activate and deactivate account [finishes #165482364]

--- server/middleware/adminStaffValidator.js
+++ server/middleware/adminStaffValidator.js
@@ -34,104 +34,25 @@ export const validCurrentAccounts = (req, res, next) => {
 
 }
 
-// export const validDeleteCurrentAccount = (req, res, next) => {
-
-//     const id = parseInt(req.params.id);
-//         let validUser;
-
-//         bankadb.currentBankAccount.map((user, index) => {
-//             if (user.id === id) {
-//                 validUser = user
-
-//                 req.body.fullName = validUser.fullName;
-//                 req.body.accountType = validUser.accountType;
-
-//                 bankadb.currentBankAccount.splice(index, 1);
-//             }
-//         });
-
-//         if(!validUser){
-//             res.status(404).send({
-//                 status: 'error', 
-//                 message: 'User not found'
-//             });
-//         }
-//         return next();
-// }
-
 export const validPatchBankAccount = (req, res, next) => {
     
+    const accountNumber = parseInt(req.params.accountNumber),
+          statusUpdate  = req.params.statusUpdate;
 
-    let { accountStatus, fullName, userAccount, userStatus } = req.body;
-
-
-                 fullName = key.full_name;
-                 userAccount = key.account_type;
-                 req.body.accountStatus = 'Dormant';
-
-                 if(accountNumber != key.account_number){
-                    res.status(404).send({
-                        status: 'error', 
-                        message: 'User not found'
-                    });
-                 }
-
-                 if(key.account_status == 'Active'){
-                    account_status = 'Dormant';
-                    userStatus = 'Dormant';
-
-                }
-                else if(key.account_status == 'Dormant'){
-                        account_status = 'Active';
-                        userStatus = 'Active';
-                }
-
-                return res.status(200).json({
-                 success: true,
-                 message: `${fullName}'s ${userAccount} Account is now ${userStatus}`,
-                 results: results.rows
-              })
-                 
-         
-
-
+    if(statusUpdate == 'activate'){
+        req.body.accountStatus = 'Active';
+    }
+    else if(statusUpdate == 'deactivate'){
+        req.body.accountStatus = 'Dormant';
+    }
+    else{
+        return res.status(200).json({
+            success: 'error',
+            message: 'Invalid Action'
+        })
+    }
     return next();
 }
-
-// export const validPatchCurrentAccount = (req, res, next) => {
-
-//     const id = parseInt(req.params.id);
-//     let validUser;
-
-//     bankadb.currentBankAccount.map((user) => {
-//         if (user.id === id) {
-//             validUser = user;
-
-//             req.body.validUser = validUser;
-//             req.body.fullName = validUser.fullName;
-//             req.body.accountType = validUser.accountType;
-//         }
-//     });
-
-//     if(!validUser){
-//         res.status(404).send({
-//             status: 'error', 
-//             message: 'User not found'
-//         });
-//     }
-
-//     if(validUser.accountStatus == 'Active'){
-//         validUser.accountStatus = 'Dormant';
-//         req.body.accountStatus = 'Dormant';
-
-//     }
-//     else if(validUser.accountStatus == 'Dormant'){
-//         validUser.accountStatus = 'Active';
-//         req.body.accountStatus = 'Active';
-//     }
-
-//     return next();
-// }
 
 export const validAdminCreateAccount = (req, res, next) => {
 
@@ -265,71 +186,23 @@ export const validDeleteStaffAccount = (req, res, next) => {
         return next();
 }
 
-export const validPatchAdminAccount = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    let validUser;
+export const validPatchUserAccount = (req, res, next) => {
+    
+    const id = parseInt(req.params.id),
+          statusUpdate  = req.params.statusUpdate;
 
-    bankadb.adminUserAccount.map((user) => {
-        if (user.id === id) {
-            validUser = user;
-
-            req.body.validUser = validUser;
-            req.body.fullName = validUser.fullName;
-            req.body.accountType = validUser.accountType;
-        }
-    });
-
-    if(!validUser){
-        res.status(404).send({
-            status: 'error', 
-            message: 'User not found'
-        });
-    }
-
-    if(validUser.accountStatus == 'Active'){
-        validUser.accountStatus = 'Dormant';
-        req.body.accountStatus = 'Dormant';
-
-    }
-    else if(validUser.accountStatus == 'Dormant'){
-        validUser.accountStatus = 'Active';
+    if(statusUpdate == 'activate'){
         req.body.accountStatus = 'Active';
     }
-
-    return next();
-}
-
-export const validPatchStaffAccount = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    let validUser;
-
-    bankadb.staffUserAccount.map((user) => {
-        if (user.id === id) {
-            validUser = user;
-
-            req.body.validUser = validUser;
-            req.body.fullName = validUser.fullName;
-            req.body.accountType = validUser.accountType;
-        }
-    });
-
-    if(!validUser){
-        res.status(404).send({
-            status: 'error', 
-            message: 'User not found'
-        });
-    }
-
-    if(validUser.accountStatus == 'Active'){
-        validUser.accountStatus = 'Dormant';
+    else if(statusUpdate == 'deactivate'){
         req.body.accountStatus = 'Dormant';
-
     }
-    else if(validUser.accountStatus == 'Dormant'){
-        validUser.accountStatus = 'Active';
-        req.body.accountStatus = 'Active';
+    else{
+        return res.status(200).json({
+            success: 'error',
+            message: 'Invalid Action'
+        })
     }
-
     return next();
 }
 
@@ -337,8 +210,6 @@ export const validupdateBankAccount = (req, res, next) => {
            
            const accountNumber = parseInt(req.params.accountNumber),
                  transactionType = req.params.transactionType;
-
-           // let {fullName, accountEmail, accountType,totalCredit, totalDebit, oldBalance, newBalance, cashier, credit, debit} = req.body;
 
             req.body.amount = parseFloat(req.body.amount);
             req.body.transactionDate = new Date();
@@ -376,61 +247,8 @@ export const validupdateBankAccount = (req, res, next) => {
                 message: 'Invalid transaction'
             });
         }
-        
-        
+         
         return next();
 }
 
-// export const validUpdateSavingsAccount = (req, res, next) => {
 
-//     const id = parseInt(req.params.id);
-//         let validUser;
-
-//         bankadb.savingsBankAccount.map((user) => {
-//             if (user.id === id) {
-//                 validUser = user;
-
-//             req.body.fullName = validUser.fullName;
-//             req.body.accountEmail = validUser.emailAddress;
-//             req.body.id = validUser.id;
-//             req.body.accountNumber = validUser.accountNumber;
-//             req.body.accountType = validUser.accountType;
-//             req.body.totalCredit = validUser.totalCredit;
-//             req.body.totalDebit = validUser.totalDebit;
-//             req.body.oldBalance = validUser.oldBalance;
-//             req.body.newBalance = validUser.newBalance
-//             }
-//         });
-
-//         if(!validUser){
-//             res.status(404).send({
-//                 status: 'error', 
-//                 message: 'User not found'
-//             });
-//         }
-
-//         if(!req.body.amount || req.body.amount < 1 || !req.body.transactionType || isNaN(req.body.cashier)){
-//             res.status(404).send({
-//                 status: 'error', 
-//                 message: 'All fields are required'
-//             });
-//         }
-        
-//         if(req.body.transactionType == 'Credit'){
-//             validUser.credit = parseFloat(req.body.amount);
-//             validUser.totalCredit = validUser.totalCredit + validUser.credit;
-//             validUser.debit = parseFloat(0);
-//             validUser.totalDebit = validUser.totalDebit + validUser.debit;
-//             validUser.oldBalance = validUser.newBalance;
-//             validUser.newBalance = validUser.totalCredit - validUser.totalDebit;
-//         }
-//         else if(req.body.transactionType == 'Debit'){
-//                 validUser.debit = parseFloat(req.body.amount);
-//                 validUser.totalDebit = validUser.totalDebit + validUser.debit; 
-//                 validUser.credit = parseFloat(0);
-//                 validUser.totalCredit = validUser.totalCredit + validUser.credit;       
-//                 validUser.oldBalance = validUser.newBalance;
-//                 validUser.newBalance = validUser.totalCredit - validUser.totalDebit;
-//         }
-//         return next();
-// }
